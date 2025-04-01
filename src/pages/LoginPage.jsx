0ddaebdee804395acb100cfb0c5b2b24f22b6b6e
Fabio <fabiolima@fabiolima.dev>
2025-03-31 21:16:09 -0300

    Adiciona estilos iniciais e começa a interface da página de login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,19 @@
+import LogoUnivesp from "../../public/logo-univesp.svg?url";
+
 function LoginPage() {
   return (
-    <>
-      <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl">
-        Tailwind CSS + Vite funcionando! 🚀
+    <div className="px-5 sm:px-40 h-full">
+      <div className="py-10 h-full flex m-auto items-center flex-col justify-around min-w-[280px] max-w-[400px] sm:m-0 sm:items-start">
+        <img src={LogoUnivesp} alt="Logo Univesp" className="w-full px-4" />
+        <h1 className="lg:text-6xl">
+          Conecte-se, ensine, aprenda e cresça junto!
+        </h1>
+        <div className="flex flex-col gap-6 w-full">
+          <button className="bg-primary text-quarternary">Entrar</button>
+          <button>Registrar</button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
