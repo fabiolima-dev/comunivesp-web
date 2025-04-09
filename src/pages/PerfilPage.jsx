13320e89f50dc9025a3ce1432fc35bf0d29e814c
jnx257 <lucca.carregal@hotmail.com>
2025-04-09 20:42:55 -0300

    adiciona informacoes do perfil (foto, dados) e tags (em breve)

--- src/pages/PerfilPage.jsx
+++ src/pages/PerfilPage.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/perfil/Header";
+import ProfileInfo from "../components/perfil/ProfileInfo";
 
 function PerfilPage() {
   return (
     <>
       <Header />
+      <ProfileInfo />
       <Outlet />
     </>
   );
